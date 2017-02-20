254b1f924acb2be5159ea7b7a03ee4a8de6f7579
Paras Dahal <shree5paras@gmail.com>
2017-02-20 20:11:05 +0530

    Cleaned Recommendation class. Added skeleton Algorithm class.

--- engine/Algorithm.py
+++ engine/Algorithm.py
@@ -1,6 +1,13 @@
 class Algorithm:
 
-    def __init__(self,datasets):
-
-        print(datasets)
+    def __init__(self,sc,datasets):
+        pass
         
+    def train(self):
+        pass
+
+    def _internal_method(self):
+        pass
+
+    def route_method(self,params):
+        pass
